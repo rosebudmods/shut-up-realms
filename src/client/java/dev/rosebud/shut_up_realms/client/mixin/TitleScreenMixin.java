af82ddd21c8a57c228c40c25bbe6ad30d081e567
ix0rai <ix0rai64@gmail.com>
2024-08-27 00:14:56 -0500

    fix random crashes

--- src/client/java/dev/rosebud/shut_up_realms/client/mixin/TitleScreenMixin.java
+++ src/client/java/dev/rosebud/shut_up_realms/client/mixin/TitleScreenMixin.java
@@ -3,6 +3,8 @@ package dev.rosebud.shut_up_realms.client.mixin;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
@@ -13,5 +15,10 @@ public class TitleScreenMixin {
 	@Overwrite
 	private boolean areRealmsNotificationsEnabled() {
 		return false;
+	}
+	
+	@Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/TitleScreen;areRealmsNotificationsEnabled()Z"))
+	public boolean areRealmsNotificationsEnabled(TitleScreen instance) {
+		return true;
 	}
 }
